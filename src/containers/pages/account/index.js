02fc229e8f2e2739755b238bab29f46f3f510307
ahmadfadhilah <stevenasep000@gmail.com>
2021-04-12 14:21:30 +0700

    Terakhir

--- src/containers/pages/account/index.js
+++ src/containers/pages/account/index.js
@@ -1,50 +1,109 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity, TouchableHighlight } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
-
+import Simple from 'react-native-vector-icons/SimpleLineIcons'
+import Help from 'react-native-vector-icons/Ionicons'
+import Clock from 'react-native-vector-icons/AntDesign'
 
 export default class Account extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.Icon}>
-                    <Icon name='arrow-back-ios' size={25}/>
-                    <Icon name='settings' size={25}/>
+                {/* profile atas  */}
+                <View style={{backgroundColor: '#ffff3d'}}>
+                    <View style={styles.Icon}>
+                        <TouchableOpacity>
+                        <Icon name='arrow-back-ios' size={25}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                        <Icon name='settings' size={25}/>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.user}>
+                        <Image source={require('../../../assets/icon/profile.png')} style={styles.userImg}/>
+                        <Text style={styles.userText}>StevenAsep</Text>
+                        <Text style={styles.userText1}>StevenAsep000@gmail.com</Text>
+                    </View>
                 </View>
-                <View style={styles.user}>
-                    <Image source={require('../../../assets/icon/profile.png')} style={styles.userImg}/>
-                    <Text style={styles.userText}>StevenAsep</Text>
-                    <Text style={styles.userText1}>StevenAsep000@gmail.com</Text>
+                {/* bar section */}
+                <View style={{height: 12, backgroundColor: '#F2F2F2'}}></View>
+                <View style={styles.name}>
+                    <View style={styles.namehal}>
+                    <Clock name="hearto" size={22} style={{paddingLeft: 5}}/>
+                    <Text style={styles.text}>Favorit saya</Text>
+                    </View>
                 </View>
-                <View>
-                    <TouchableOpacity>
-                        <Text>apa weh</Text>
-                    </TouchableOpacity>
+                <View style={styles.name}>
+                    <View style={styles.namehal}>
+                    <Help name="clipboard-outline" size={22} style={{paddingLeft: 5}}/>
+                    <Text style={styles.text}>Pesanan saya</Text>
+                    </View>
+                </View>
+                <View style={styles.name}>
+                    <View style={styles.namehal}>
+                    <Clock name="staro" size={22} style={{paddingLeft: 5}}/>
+                    <Text style={styles.text}>Beri Penilaian</Text>
+                    </View>
+                </View>
+                <View style={styles.name}>
+                    <View style={styles.namehal}>
+                    <Clock name="message1" size={22} style={{paddingLeft: 5}}/>
+                    <Text style={styles.text}>Ulasan saya</Text>
+                    </View>
+                </View>
+                <View style={styles.name}>
+                    <View style={styles.namehal1}>
+                    <Clock name="clockcircleo" size={21} style={{paddingLeft: 5}}/>
+                    <Text style={styles.text}>Terakhir dilihat</Text>
+                    </View>
+                </View>
+                {/* Help */}
+                <View style={{height: 12, backgroundColor: '#F2F2F2'}}></View>
+                <View style={styles.name}>
+                    <View style={styles.namehal}>
+                    <Simple name="user" size={20} style={{paddingLeft: 5}}/>
+                    <Text style={styles.text}>Pengaturan Account</Text>
+                    </View>
+                </View>
+                <View style={styles.name}>
+                    <View style={styles.namehal}>
+                    <Help name="help-outline" size={22} style={{paddingLeft: 5}}/>
+                    <Text style={styles.text}>Pusat Bantuan</Text>
+                    </View>
+                </View>
+                <View style={styles.name}>
+                    <View style={styles.namehal1}>
+                    <Simple name="logout" size={20} style={{paddingLeft: 5}}/>
+                    <Text style={styles.text}>Log out</Text>
+                    </View>
                 </View>
             </View>
         )
     }
 }
 
+// styles
+
 const styles = StyleSheet.create ({
     container: {
         flex: 1,
         backgroundColor: 'white',
-        marginTop: 12
     },
     Icon: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginHorizontal: 16
+        marginHorizontal: 16,
+        marginTop: 12,
     },
     user: {
         alignItems: 'center',
-
+        marginBottom: 14,
+        marginTop: -18
     },
     userImg: {
         width: 130,
         height: 130,
-        marginTop: 60,
+        marginTop: 40,
     },
     userText: {
         fontSize: 17,
@@ -55,5 +114,22 @@ const styles = StyleSheet.create ({
         fontSize: 12,
         color: 'grey',
         paddingTop: 3
+    },
+    name: {
+        marginTop: 12,
+    },
+    namehal: {
+        borderBottomWidth: 1,
+        borderBottomColor: 'grey',
+        marginLeft: 20,
+        flexDirection: 'row',
+    },
+    namehal1: {
+        flexDirection: 'row',
+        marginLeft: 20
+    },
+    text: {
+        marginLeft: 14,
+        marginBottom: 12
     }
 })
