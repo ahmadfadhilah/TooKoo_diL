ff7f83103ca447b5c0eefc09038ee53b71306d7a
ahmadfadhilah <stevenasep000@gmail.com>
2022-07-23 13:14:54 +0700

    update

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
-// import React, {Component} from 'react';
-// import Router from './src/config/router';
+import React, {Component} from 'react';
+import Router from './src/config/router';
 
-// export class App extends Component {
-//   render() {
-//     return <Router />;
-//   }
-// }
-
-// export default App;
-
-import {View, Text} from 'react-native';
-import React from 'react';
-
-const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  );
-};
+export class App extends Component {
+  render() {
+    return <Router />;
+  }
+}
 
 export default App;
