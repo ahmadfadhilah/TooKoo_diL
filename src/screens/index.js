cac194802ca62a770d5dfeb8c19cbe66077041ab
ahmadfadhilah <stevenasep000@gmail.com>
2022-07-23 07:52:27 +0700

    project repair

---
+++ src/screens/index.js
@@ -0,0 +1,49 @@
+import SplashScreen from './Auth/SplashScreen';
+import Login from './Auth/Login';
+import Register from './Auth/Register';
+import ForgotPassword from './Auth/ForgotPassword';
+import NavTobTab from './Auth/NavTobTab';
+import Chat from './Chat/Chat';
+import Dashboard from './Home/Dashboard';
+import Keranjang from './Home/Keranjang';
+import Kontak from './Home/Kontak';
+import Message from './Home/Message';
+import NewsDetail from './Home/NewsDetail';
+import Notivikasi from './Notifications/Notivikasi';
+import barNotiv from './Notifications/barNotiv';
+import BelumBayar from './Order/BelumBayar';
+import Dikirim from './Order/Dikirim';
+import NavTobOrder from './Order/NavTobOrder';
+import Order from './Order/Order';
+import Account from './User/Account';
+import NotAccount from './User/NotAccount';
+import Pesanan from './User/Pesanan';
+import Setting from './User/Setting';
+import SettingProfile from './User/SettingProfile';
+import pembeda from './User/pembeda';
+
+export {
+    SplashScreen,
+    Login,
+    Register,
+    ForgotPassword,
+    NavTobTab,
+    Chat,
+    Dashboard,
+    Keranjang,
+    Kontak,
+    Message,
+    NewsDetail,
+    Notivikasi,
+    barNotiv,
+    BelumBayar,
+    Dikirim,
+    NavTobOrder,
+    Order,
+    Account,
+    NotAccount,
+    Pesanan,
+    Setting,
+    SettingProfile,
+    pembeda
+}
